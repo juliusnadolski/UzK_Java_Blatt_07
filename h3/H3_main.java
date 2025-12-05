51e41ebaab1dabe46a6e6edf97d3945a39aaa3aa
juliusnadolski <julius.nadolski.uni@gmail.com>
2025-12-05 19:44:30 +0100

    second_push

--- h3/H3_main.java
+++ h3/H3_main.java
@@ -2,4 +2,16 @@ package h3;
 
 public class H3_main {
 
+    public static void main(String[] args) {
+        Mensch person = new Mensch();
+        
+        person.setName("Julius");
+        person.setGebJahr(2005);
+
+        person.setAlter();
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Geburtsjahr: " + person.getGebJahr());
+        System.out.println("Alter (in 2025): " + person.getAlter());
+    }
 }
